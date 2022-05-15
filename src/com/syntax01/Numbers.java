050b3413cf2c944bd3195d43d228fa3e9a746cfc
acca5859 <acca5859@gmail.com>
2022-05-15 14:41:09 +0300

    first commit

---
+++ src/com/syntax01/Numbers.java
@@ -0,0 +1,8 @@
+package com.syntax01;
+public class Numbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + ";");
+        }
+    }
+}
